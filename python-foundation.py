2cfdd1d6e8c3bb411d3195c2d67658b2fafc8350
Cole Frederick <u0xee@rams.colostate.edu>
2017-01-29 12:26:17 -0700

    First lines

---
+++ python-foundation.py
@@ -0,0 +1,28 @@
+# Python basics
+# in ipython3
+
+
+# from the shell, type 'ipython3' and hit enter to start the interpreter
+denver:~$ ipython3<enter>
+Python 3.5.2+ (default, Sep 22 2016, 12:18:14)
+
+IPython 2.4.1 -- An enhanced Interactive Python.
+?         -> Introduction and overview of IPython's features.
+%quickref -> Quick reference.
+help      -> Python's own help system.
+object?   -> Details about 'object', use 'object??' for extra details.
+
+# notice the different ways you can inspect objects or get help
+# try ?, and type 'q' when you want to leave the introduction
+
+
+# we exit the interpreter with Ctrl-D or by running the exit function
+In [1]: exit()
+# now we are back at the shell, let's get back in
+
+
+In [1]: x = 5 # variables hold any type
+
+In [2]: x ** 100 # x raised to the hundred
+Out[2]: 7888609052210118054117285652827862296732064351090230047702789306640625
+
